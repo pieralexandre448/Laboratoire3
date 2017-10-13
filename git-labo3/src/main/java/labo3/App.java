58cc06bcb38c7890a1a8fd3885ded4362348d164
Pier-Alexandre Yale <starwarsgalactic@gmail.com>
2017-10-13 17:41:27 -0400

    Importing the git-demo project

--- git-labo3/src/main/java/labo3/App.java
+++ git-labo3/src/main/java/labo3/App.java
@@ -1,19 +1,13 @@
 package labo3;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@SpringBootApplication
-@RestController
-public class App {
-    public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
-    }
-
-    @RequestMapping(value = "/")
-    String hello() {
-        return "Hello World!";
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
     }
 }
